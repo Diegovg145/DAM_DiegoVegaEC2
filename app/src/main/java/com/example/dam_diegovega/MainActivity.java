be48e22ddfb753f65ba5865e787b2ac0f16c5671
Diegovg145 <20b3110312@untels.edu.pe>
2024-05-15 11:17:09 -0500

    EC2 Terminado

--- app/src/main/java/com/example/dam_diegovega/MainActivity.java
+++ app/src/main/java/com/example/dam_diegovega/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.dam_diegovega;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.envio);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envio tus datos", Toast.LENGTH_SHORT).show());
+
+        TextView tv2 = findViewById(R.id.envio);
+        tv2.setOnClickListener(view -> Toast.makeText(this, "Se envio tus datos", Toast.LENGTH_SHORT).show());
     }
 }
